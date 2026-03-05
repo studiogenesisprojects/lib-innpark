1577332c19fb284dd2bbcbdaf639d3bb9a20c7f5
Ferter3006 <ferratersimon@gmail.com>
2026-03-05 11:58:03 +0100

    delete vehicle at offstreet-call

--- offstreet_service.go
+++ offstreet_service.go
@@ -12,7 +12,7 @@ var offstreetUrl = os.Getenv("API_OFFSTREET_URL")
 
 func CreateVehicle(plate string, userId string) (string, error) {
 	// Create vehicle
-	url := fmt.Sprintf("%s/v1/vehicles", offstreetUrl)
+	url := fmt.Sprintf("%s/v1/vehicles/create", offstreetUrl)
 
 	body := strings.NewReader(fmt.Sprintf(`{	
 		"plate": "%s",
@@ -44,6 +44,42 @@ func CreateVehicle(plate string, userId string) (string, error) {
 	}
 
 	return createVehicleResponse.Id, nil
+}
+
+func DeleteVehicle(plate string, userId string) error {
+	// delete vehicle
+	url := fmt.Sprintf("%s/v1/vehicles/delete", offstreetUrl)
+
+	body := strings.NewReader(fmt.Sprintf(`{	
+		"plate": "%s",
+		"user_id": "%s"
+	}`, plate, userId))
+
+	req, err := http.NewRequest("POST", url, body)
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	response, err := client.Do(req)
+
+	if err != nil {
+		return err
+	}
+
+	if response.StatusCode != 200 {
+		return fmt.Errorf("api-offstreet-error: %d", response.StatusCode)
+	}
+
+	return nil
+}
+
+type VehicleResponse struct {
+	Id     string `json:"id"`
+	UserId string `json:"user_id"`
+	Plate  string `json:"plate"`
 }
 
 type CreateVehicleResponse struct {
