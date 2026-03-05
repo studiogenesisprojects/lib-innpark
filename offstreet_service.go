2825184c19a5ab9a8319225308e4276d3f273199
Ferter3006 <ferratersimon@gmail.com>
2026-03-05 08:33:01 +0100

    offstreet service with createVehicle()

---
+++ offstreet_service.go
@@ -0,0 +1,50 @@
+package innpark
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+)
+
+var offstreetUrl = os.Getenv("API_OFFSTREET_URL")
+
+func CreateVehicle(plate string, userId string) (string, error) {
+
+	url := fmt.Sprintf("%s/v1/vehicles", offstreetUrl)
+
+	body := strings.NewReader(fmt.Sprintf(`{	
+		"plate": "%s",
+		"user_id": "%s"
+	}`, plate, userId))
+
+	req, err := http.NewRequest("POST", url, body)
+	if err != nil {
+		return "", err
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	response, err := client.Do(req)
+
+	if err != nil {
+		return "", err
+	}
+
+	if response.StatusCode != 200 {
+		return "", fmt.Errorf("api-offstreet-error: %d", response.StatusCode)
+	}
+
+	createVehicleResponse := &CreateVehicleResponse{}
+	err = json.NewDecoder(response.Body).Decode(createVehicleResponse)
+	if err != nil {
+		return "", fmt.Errorf("api-offstreet-error: %s", err.Error())
+	}
+
+	return createVehicleResponse.Id, nil
+}
+
+type CreateVehicleResponse struct {
+	Id string `json:"id"`
+}
